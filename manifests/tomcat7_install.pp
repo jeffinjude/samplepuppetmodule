323c58638e9f76d5e3376fb9a564d839868e59f0
root <root@ip-172-31-5-63.us-west-1.compute.internal>
2016-11-16 09:50:18 +0000

    added specific tomcat configuration for production

--- manifests/tomcat7_install.pp
+++ manifests/tomcat7_install.pp
@@ -10,5 +10,25 @@ class samplepuppetmodule::tomcat7_install{
   hasrestart => true,
   require => Package['tomcat7']
   }
+
+  if $::ipaddress == '172.31.7.57'
+  {
+  file { '/var/lib/tomcat7/conf/server.xml' :
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    source  => 'puppet:///modules/samplepuppetmodule/server.xml',
+    require => Package['tomcat7'],
+    notify => Service['tomcat7']
+  }
+  file { '/etc/default/tomcat7' :
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    source  => 'puppet:///modules/samplepuppetmodule/tomcat7',
+    require => Package['tomcat7'],
+    notify => Service['tomcat7']
+  }
+  }
 }
 
